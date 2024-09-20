e4aeca9aa370bde6372fccf7cdd5d3683ee4fec2
Suresh Rokaya <rokayasuresh028@gmail.com>
2024-09-20 16:21:10 +0545

    paginator

--- apps/roles/views.py
+++ apps/roles/views.py
@@ -10,14 +10,27 @@ from django.views import View
 from django.shortcuts import render, redirect
 from .models import Roles
 from .forms import RolesForm
-
+from django.core.paginator import Paginator
 class RolesView(View):
     template_name = 'roles/roles.html'
 
     def get(self, request):
         roles = Roles.objects.all()  
         form = RolesForm() 
-        return render(request, self.template_name, {'data': roles, 'form': form})
+       
+
+        paginator = Paginator(roles, 2)
+        page_number = request.GET.get('page')
+        servicedatafinal = paginator.get_page(page_number)
+        totalpage = servicedatafinal.paginator.num_pages
+        
+        return render(request, self.template_name, {
+            'roles': servicedatafinal, 
+            'form': form, 
+            'lastpage': totalpage,
+            'totalPagelist': [n+1 for n in range(totalpage)],
+        })
+
 
     def post(self, request):
         form = RolesForm(request.POST)  
