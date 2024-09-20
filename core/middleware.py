9bd3dc6c60fbae77d670e91bc960569144e9fb31
Suresh Rokaya <rokayasuresh028@gmail.com>
2024-09-20 09:21:11 +0545

    Database table crete

--- core/middleware.py
+++ core/middleware.py
@@ -8,11 +8,13 @@ class RedirectIfNotLoggedInMiddleware:
     def __call__(self, request):
         login_url = reverse('login')
         register_url = reverse('register') 
-        role_url = reverse('role') 
+        # role_url = reverse('role') 
         
         # Redirect unauthenticated users trying to access any page except login or register
-        if not request.user.is_authenticated:
-            if request.path not in [login_url, register_url, role_url]:
+        if request.path =='':
+            return redirect('login')
+        elif not request.user.is_authenticated:
+            if request.path not in [login_url, register_url]:
                 return redirect('login')
         
         # Redirect authenticated users from login page to dashboard
--- role/models.py
+++ role/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Role(models.Model):
     role_name=models.CharField(max_length=100)
     descriptions=models.TextField()
-    isRole = models.BooleanField(default=False)
+    isRole = models.BooleanField(default=False, verbose_name='isRole')
 
