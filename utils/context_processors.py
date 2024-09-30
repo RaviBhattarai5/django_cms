3b7260e735bf30ef0af8e691680a948cf2f01dd5
Ravi Bhattarai <ravibhattarai5@gmail.com>
2024-09-30 16:08:07 +0545

    extract only permitted menus

--- utils/context_processors.py
+++ utils/context_processors.py
@@ -1,24 +1,49 @@
 from apps.menu.models import Menu
+from apps.users.models import UserRole
+from apps.permission_type.models import PermissionType
 
 def menu_context(request):
-    top_level_menus = Menu.objects.filter(parent__isnull=True)
-    menu_hierarchy = []
+    if not request.user.is_authenticated:
+        return {'menu_hierarchy': []}
+    
+    user = request.user
 
-    for menu in top_level_menus:
-        menu_hierarchy.append({
+    if user.is_superuser:
+        top_level_menus = Menu.objects.filter(parent__isnull=True).prefetch_related('children')
+    else:
+        user_roles = UserRole.objects.filter(user=user).values_list('role_id', flat=True)
+        browse_permission = PermissionType.objects.get(name='Browse')
+        
+        top_level_menus = Menu.objects.filter(
+            parent__isnull=True,
+            setpermission__role_id__in=user_roles,
+            setpermission__permission_type=browse_permission
+        ).distinct().prefetch_related('children')
+
+    menu_hierarchy = [
+        {
             'menu': menu,
-            'children': get_children(menu)
-        })
+            'children': get_children(menu, user, user_roles if not user.is_superuser else None, browse_permission if not user.is_superuser else None)
+        }
+        for menu in top_level_menus
+    ]
+    
     return {'menu_hierarchy': menu_hierarchy}
 
-def get_children(menu):
-    children = menu.children.all()
-    children_hierarchy = []
 
-    for child in children:
-        children_hierarchy.append({
+def get_children(menu, user, user_roles=None, browse_permission=None):
+    if user.is_superuser:
+        children = menu.children.all()
+    else:
+        children = menu.children.filter(
+            setpermission__role_id__in=user_roles,
+            setpermission__permission_type=browse_permission
+        ).distinct()
+
+    return [
+        {
             'menu': child,
-            'children': get_children(child)
-        })
-
-    return children_hierarchy
+            'children': get_children(child, user, user_roles, browse_permission)
+        }
+        for child in children
+    ]
