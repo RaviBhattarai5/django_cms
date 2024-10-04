66ad5eaea7736677326097b689742fe692c7bae6
Ravi Bhattarai <ravibhattarai5@gmail.com>
2024-10-04 09:28:40 +0545

    working

--- apps/menu/views.py
+++ apps/menu/views.py
@@ -82,7 +82,7 @@ class MenuUpdateView(UpdateView):
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['page_title'] = 'Create Menu'
+        context['page_title'] = 'Update Menu'
         context['breadcrumbs'] = [{'name':'Dashboard', 'url':'dashboard'},{'name':'Menu', 'url':'menu_list'},{'name':'Update Menu'}]
         return context
     
