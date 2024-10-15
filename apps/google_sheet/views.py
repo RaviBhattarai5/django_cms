e9b9d75cc34543d8ec0f739471f1b4df69f41bb7
Ravi Bhattarai <ravibhattarai5@gmail.com>
2024-10-15 09:36:19 +0545

    working on sheet

--- apps/google_sheet/views.py
+++ apps/google_sheet/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from utils.google_sheet import connect_sheet
+from utils.google_sheet import fetch_data_in_batches
 from django.contrib import messages
 import logging
 
@@ -9,14 +9,16 @@ def get_sheet_data(request):
     datas = []
     headers = []
     try:
-        datas = connect_sheet()
-        headers = datas.pop(0)
+        # datas = connect_sheet()
+        # headers = datas.pop(0)
+        fetch_data_in_batches()
+        print('test')
     except Exception as e:
         logger.error(f"An error occurred: {e}")
         messages.warning(request, e)
     return render(request, 'google_sheet/index.html', {
-        'datas': datas,
-        'headers': headers,
+        'datas': [],
+        'headers': [],
         'page_title': 'Google Sheet'
     })
     
--- utils/google_sheet.py
+++ utils/google_sheet.py
@@ -4,12 +4,38 @@ from googleapiclient.discovery import build
 SPREADSHEET_ID = '1pmG--Geo0Uc5sPvJg7le8IsD4aLPbK3-gpEgJAzDBlw'
 SERVICE_ACCOUNT_FILE = 'credentials/google_acc_key.json'
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+RANGE_NAME = 'intersted'
 
 def connect_sheet():
-    RANGE_NAME = 'intersted!A:I'
     credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
     service = build('sheets', 'v4', credentials=credentials)
-    
+    return service
+
+
+def fetch_sheet_data(spreadsheet_id=SPREADSHEET_ID, range_name=RANGE_NAME):
+    service = connect_sheet()
     sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
+    result = sheet.values().get(spreadsheetId=spreadsheet_id, range=range_name).execute()
     return result.get('values', [])
+
+
+def get_total_rows_from_metadata(spreadsheet_id=SPREADSHEET_ID, sheet_name=RANGE_NAME):
+    service = connect_sheet()
+    spreadsheet_metadata = service.spreadsheets().get(spreadsheetId=spreadsheet_id).execute()
+    sheet_metadata = next(sheet for sheet in spreadsheet_metadata['sheets'] if sheet['properties']['title'] == sheet_name)
+    total_rows = sheet_metadata['properties']['gridProperties']['rowCount']
+    return total_rows
+
+
+def fetch_data_in_batches(spreadsheet_id=SPREADSHEET_ID, sheet_name=RANGE_NAME, start_row=1, batch_size=500, last_col='X'):
+    service = connect_sheet()
+    current_row = start_row
+    total_rows = get_total_rows_from_metadata(spreadsheet_id, sheet_name)
+    
+    while current_row <= total_rows:
+        end_row = min(current_row + batch_size - 1, total_rows)
+        range_name = f"{sheet_name}!A{current_row}:{last_col}{end_row}"
+        result = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_name).execute()
+        values = result.get('values', [])
+        print(values)
+        current_row += batch_size
